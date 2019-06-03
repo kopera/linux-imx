d3b52017b7957cb5f32760cf845e913945063575
Ali Sabil <ali.sabil@koperadev.com>
2019-06-03 13:56:53 +0200

    ASoC: codecs: TAS5825M simplify regmap by avoiding DSP registers

--- sound/soc/codecs/tas5825m.c
+++ sound/soc/codecs/tas5825m.c
@@ -1,17 +1,7 @@
-/*
- * Driver for the TAS5825M Audio Amplifier
- *
- * Author: Andy Liu <andy-liu@ti.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- */
+// SPDX-License-Identifier: GPL-2.0
+// Audio driver for TAS5825M Audio Amplifier
+// Copyright (C) 2019 KOPERA
+// Ali Sabil <ali.sabil@koperadev.com>
 
 #include <linux/types.h>
 #include <linux/module.h>
@@ -39,126 +29,51 @@ static const char * const tas5825m_supply_names[2] = {
 	"pvdd",    /* Class-D amp and analog power supply (connected). */
 };
 
-
 struct tas5825m_priv {
-	struct mutex  lock;
-
 	struct regmap *regmap;
 	struct regulator_bulk_data supplies[TAS5825M_NUM_SUPPLIES];
+};
 
-	size_t volume_l_index;
-	size_t volume_r_index;
+static bool tas5825m_is_writable_reg(struct device *device, unsigned int reg)
+{
+	return reg != 0x00 && reg != 0x7f;
+}
 
-	bool muted;
+
+static bool tas5825m_is_readable_reg(struct device *device, unsigned int reg)
+{
+	return reg != 0x00 && reg != 0x7f;
+}
+
+
+static bool tas5825m_is_volatile_reg(struct device *device, unsigned int reg)
+{
+	return reg == 0x00 || reg == 0x7f;
+}
+
+
+static const struct regmap_config tas5825m_regmap_config = {
+	.reg_bits = 8,
+	.val_bits = 8,
+
+	.max_register = 0x78,
+	.writeable_reg = tas5825m_is_writable_reg,
+	.readable_reg = tas5825m_is_readable_reg,
+	.volatile_reg = tas5825m_is_volatile_reg,
+
+	.cache_type = REGCACHE_RBTREE,
 };
 
 
-static int tas5825m_speaker_volume_get(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
-
-	mutex_lock(&priv->lock);
-	ucontrol->value.integer.value[0] = priv->volume_l_index;
-	ucontrol->value.integer.value[1] = priv->volume_r_index;
-	mutex_unlock(&priv->lock);
-
-	dev_dbg(codec->dev, "%s(): volume=(%ld, %ld)\n", __func__, priv->volume_l_index, priv->volume_r_index);
-
-	return 0;
-}
-
-static int tas5825m_speaker_volume_put(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
-	size_t l = ucontrol->value.integer.value[0];
-	size_t r = ucontrol->value.integer.value[1];
-
-	size_t volume_l_index = clamp(l, TAS5825M_VOLUME_MIN, TAS5825M_VOLUME_MAX);
-	uint32_t volume_l = tas5825m_volume[volume_l_index];
-	uint8_t volume_l4 = ((volume_l >> 24) & 0xff);
-	uint8_t volume_l3 = ((volume_l >> 16) & 0xff);
-	uint8_t volume_l2 = ((volume_l >>  8) & 0xff);
-	uint8_t volume_l1 = ((volume_l >>  0) & 0xff);
-
-	size_t volume_r_index = clamp(r, TAS5825M_VOLUME_MIN, TAS5825M_VOLUME_MAX);
-	uint32_t volume_r = tas5825m_volume[volume_r_index];
-	uint8_t volume_r4 = ((volume_r >> 24) & 0xff);
-	uint8_t volume_r3 = ((volume_r >> 16) & 0xff);
-	uint8_t volume_r2 = ((volume_r >>  8) & 0xff);
-	uint8_t volume_r1 = ((volume_r >>  0) & 0xff);
-
-	dev_dbg(codec->dev, "%s(volume=(%lu, %lu))\n", __func__, volume_l_index, volume_r_index);
-
-	mutex_lock(&priv->lock);
-
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, 0x00);
-	snd_soc_write(codec, TAS5825M_REG_BOOK_CTRL, 0x8c);
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, 0x0b);
-
-	snd_soc_write(codec, 0x0c, volume_l4);
-	snd_soc_write(codec, 0x0d, volume_l3);
-	snd_soc_write(codec, 0x0e, volume_l2);
-	snd_soc_write(codec, 0x0f, volume_l1);
-	priv->volume_l_index = volume_l_index;
-
-	snd_soc_write(codec, 0x10, volume_r4);
-	snd_soc_write(codec, 0x11, volume_r3);
-	snd_soc_write(codec, 0x12, volume_r2);
-	snd_soc_write(codec, 0x13, volume_r1);
-	priv->volume_r_index = volume_r_index;
-
-	mutex_unlock(&priv->lock);
-
-	return 1;
-}
-
-static int tas5825m_speaker_switch_get(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
-
-	mutex_lock(&priv->lock);
-	ucontrol->value.integer.value[0] = !priv->muted;
-	mutex_unlock(&priv->lock);
-
-	return 0;
-}
-
-static int tas5825m_speaker_switch_put(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
-
-	int value = ucontrol->value.integer.value[0];
-	bool muted = value == 0;
-
-	mutex_lock(&priv->lock);
-
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_write(codec, TAS5825M_REG_BOOK_CTRL, TAS5825M_BOOK_00);
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_update_bits(codec, TAS5825M_REG_DEVICE_CTRL2,
-	                    TAS5825M_MUTE_MASK,
-	                    muted ? TAS5825M_MUTE_MUTED : TAS5825M_MUTE_UNMUTED);
-
-	priv->muted = muted;
-
-	mutex_unlock(&priv->lock);
-
-	return 1;
-}
-
 static const DECLARE_TLV_DB_SCALE(tas5825m_speaker_volume_db_scale, -10350, 50, 1);
 static const struct snd_kcontrol_new tas5825m_snd_controls[] = {
-	SOC_DOUBLE_R_EXT_TLV("Speaker Playback Volume",
-	                     0x0c, 0x10, 0, TAS5825M_VOLUME_MAX, 0,
-						 tas5825m_speaker_volume_get, tas5825m_speaker_volume_put,
+	SOC_SINGLE_RANGE_TLV("Speaker Playback Volume",
+	                     TAS5825M_REG_DIG_VOL,
+	                     0, 0x30, 0xff, 1,
 	                     tas5825m_speaker_volume_db_scale),
-	SOC_SINGLE_EXT("Speaker Playback Switch",
-	               TAS5825M_REG_DEVICE_CTRL2, 3, 1, 0,
-	               tas5825m_speaker_switch_get, tas5825m_speaker_switch_put),
+	SOC_SINGLE("Speaker Playback Switch",
+	           TAS5825M_REG_DEVICE_CTRL2,
+	           3, 1, 1),
 };
 
 static struct snd_soc_codec_driver soc_codec_dev_tas5825m = {
@@ -174,23 +89,22 @@ static int tas5825m_dai_hw_params(struct snd_pcm_substream *substream,
                              struct snd_soc_dai *dai)
 {
 	struct snd_soc_codec *codec = dai->codec;
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
 	unsigned int rate = params_rate(params);
 	unsigned int width = params_width(params);
-	u8 fsmode = TAS5825M_FSMODE_AUTO;
-	u8 word_length = TAS5825M_WORD_LENGTH_24;
+	u8 fsmode = TAS5825M_REG_SIG_CH_CTRL_FSMODE_AUTO;
+	u8 word_length = TAS5825M_REG_SAP_CTRL1_WORD_LENGTH_24;
 
 	dev_dbg(codec->dev, "%s() rate=%u width=%u\n", __func__, rate, width);
 
 	switch (rate) {
 	case 44100:
-		fsmode = TAS5825M_FSMODE_44_1KHZ;
+		fsmode = TAS5825M_REG_SIG_CH_CTRL_FSMODE_44_1KHZ;
 		break;
 	case 48000:
-		fsmode = TAS5825M_FSMODE_48KHZ;
+		fsmode = TAS5825M_REG_SIG_CH_CTRL_FSMODE_48KHZ;
 		break;
 	case 96000:
-		fsmode = TAS5825M_FSMODE_96KHZ;
+		fsmode = TAS5825M_REG_SIG_CH_CTRL_FSMODE_96KHZ;
 		break;
 	default:
 		dev_err(codec->dev, "unsupported sample rate: %u\n", rate);
@@ -199,35 +113,28 @@ static int tas5825m_dai_hw_params(struct snd_pcm_substream *substream,
 
 	switch (width) {
 	case 16:
-		word_length = TAS5825M_WORD_LENGTH_16;
+		word_length = TAS5825M_REG_SAP_CTRL1_WORD_LENGTH_16;
 		break;
 	case 20:
-		word_length = TAS5825M_WORD_LENGTH_20;
+		word_length = TAS5825M_REG_SAP_CTRL1_WORD_LENGTH_20;
 		break;
 	case 24:
-		word_length = TAS5825M_WORD_LENGTH_24;
+		word_length = TAS5825M_REG_SAP_CTRL1_WORD_LENGTH_24;
 		break;
 	case 32:
-		word_length = TAS5825M_WORD_LENGTH_32;
+		word_length = TAS5825M_REG_SAP_CTRL1_WORD_LENGTH_32;
 		break;
 	default:
 		dev_err(codec->dev, "unsupported sample width: %u\n", width);
 		return -EINVAL;
 	}
 
-	mutex_lock(&priv->lock);
-
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_write(codec, TAS5825M_REG_BOOK_CTRL, TAS5825M_BOOK_00);
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
 	snd_soc_update_bits(codec, TAS5825M_REG_SIG_CH_CTRL,
-	                    TAS5825M_FSMODE_MASK,
+	                    TAS5825M_REG_SIG_CH_CTRL_FSMODE_MASK,
 	                    fsmode);
 	snd_soc_update_bits(codec, TAS5825M_REG_SAP_CTRL1,
-	                    TAS5825M_WORD_LENGTH_MASK,
+	                    TAS5825M_REG_SAP_CTRL1_WORD_LENGTH_MASK,
 	                    word_length);
-
-	mutex_unlock(&priv->lock);
 
 	return 0;
 }
@@ -236,9 +143,9 @@ static int tas5825m_dai_hw_params(struct snd_pcm_substream *substream,
 static int tas5825m_dai_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
 {
 	struct snd_soc_codec *codec = dai->codec;
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
-	u8 sclk_inv = TAS5825M_SCLK_INV_NORMAL;
-	u8 data_format = TAS5825M_DATA_FORMAT_I2S;
+	uint8_t sclk_inv = TAS5825M_REG_I2S_CTRL_SCLK_INV_NORMAL;
+	uint8_t data_format = TAS5825M_REG_SAP_CTRL1_DATA_FORMAT_I2S;
+	int ret;
 
 	dev_dbg(codec->dev, "%s() fmt=0x%0x\n", __func__, fmt);
 
@@ -254,10 +161,10 @@ static int tas5825m_dai_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
 	/* signal polarity */
 	switch (fmt & SND_SOC_DAIFMT_INV_MASK) {
 	case SND_SOC_DAIFMT_NB_NF:
-		sclk_inv = TAS5825M_SCLK_INV_NORMAL;
+		sclk_inv = TAS5825M_REG_I2S_CTRL_SCLK_INV_NORMAL;
 		break;
 	case SND_SOC_DAIFMT_IB_IF:
-		sclk_inv = TAS5825M_SCLK_INV_INVERTED;
+		sclk_inv = TAS5825M_REG_I2S_CTRL_SCLK_INV_INVERTED;
 		break;
 	default:
 		dev_err(codec->dev, "Invalid DAI clock signal polarity\n");
@@ -267,67 +174,66 @@ static int tas5825m_dai_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
 	/* interface format */
 	switch (fmt & SND_SOC_DAIFMT_FORMAT_MASK) {
 	case SND_SOC_DAIFMT_I2S:
-		data_format = TAS5825M_DATA_FORMAT_I2S;
+		data_format = TAS5825M_REG_SAP_CTRL1_DATA_FORMAT_I2S;
 		break;
 	case SND_SOC_DAIFMT_DSP_A:
-		data_format = TAS5825M_DATA_FORMAT_DSP;
+		data_format = TAS5825M_REG_SAP_CTRL1_DATA_FORMAT_DSP;
 		break;
 	case SND_SOC_DAIFMT_RIGHT_J:
-		data_format = TAS5825M_DATA_FORMAT_RTJ;
+		data_format = TAS5825M_REG_SAP_CTRL1_DATA_FORMAT_RTJ;
 		break;
 	case SND_SOC_DAIFMT_LEFT_J:
-		data_format = TAS5825M_DATA_FORMAT_LTJ;
+		data_format = TAS5825M_REG_SAP_CTRL1_DATA_FORMAT_LTJ;
 		break;
 	default:
 		dev_err(codec->dev, "Invalid DAI interface format\n");
 		return -EINVAL;
 	}
 
-	mutex_lock(&priv->lock);
+	ret = snd_soc_update_bits(codec, TAS5825M_REG_I2S_CTRL,
+	                          TAS5825M_REG_I2S_CTRL_SCLK_INV_MASK,
+	                          sclk_inv);
 
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_write(codec, TAS5825M_REG_BOOK_CTRL, TAS5825M_BOOK_00);
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_update_bits(codec, TAS5825M_REG_I2S_CTRL,
-	                    TAS5825M_SCLK_INV_MASK,
-	                    sclk_inv);
-	snd_soc_update_bits(codec, TAS5825M_REG_SAP_CTRL1,
-	                    TAS5825M_DATA_FORMAT_MASK,
-	                    data_format);
+	if (ret < 0)
+		return ret;
 
-	mutex_unlock(&priv->lock);
+	ret = snd_soc_update_bits(codec, TAS5825M_REG_SAP_CTRL1,
+	                          TAS5825M_REG_SAP_CTRL1_DATA_FORMAT_MASK,
+	                          data_format);
 
-	return 0;
+	if (ret >= 0)
+		return 0;
+	else
+		return ret;
 }
+
 
 static int tas5825m_dai_mute(struct snd_soc_dai *dai, int mute)
 {
 	struct snd_soc_codec *codec = dai->codec;
-	struct tas5825m_priv *priv = snd_soc_codec_get_drvdata(codec);
+	int ret;
 
-	mutex_lock(&priv->lock);
+	ret = snd_soc_update_bits(codec, TAS5825M_REG_DEVICE_CTRL2,
+	                          TAS5825M_REG_DEVICE_CTRL2_MUTE_MASK,
+	                          mute ? TAS5825M_REG_DEVICE_CTRL2_MUTED : TAS5825M_REG_DEVICE_CTRL2_UNMUTED);
 
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_write(codec, TAS5825M_REG_BOOK_CTRL, TAS5825M_BOOK_00);
-	snd_soc_write(codec, TAS5825M_REG_PAGE_CTRL, TAS5825M_PAGE_00);
-	snd_soc_update_bits(codec, TAS5825M_REG_DEVICE_CTRL2,
-	                    TAS5825M_MUTE_MASK,
-	                    mute ? TAS5825M_MUTE_MUTED : TAS5825M_MUTE_UNMUTED);
-
-	mutex_unlock(&priv->lock);
-
-	return 0;
+	if (ret >= 0)
+		return 0;
+	else
+		return ret;
 }
 
-static const struct snd_soc_dai_ops tas5825m_speaker_dai_ops = {
+
+static const struct snd_soc_dai_ops tas5825m_dai_ops = {
 	.hw_params      = tas5825m_dai_hw_params,
 	.set_fmt        = tas5825m_dai_set_fmt,
 	.digital_mute   = tas5825m_dai_mute,
 };
 
+
 static struct snd_soc_dai_driver tas5825m_dai[] = {
 	{
-		.name = "tas5825m-amplifier",
+		.name = "tas5825m-hifi",
 		.playback = {
 			.stream_name  = "Playback",
 			.channels_min = 1,
@@ -339,52 +245,33 @@ static struct snd_soc_dai_driver tas5825m_dai[] = {
 			                 SNDRV_PCM_FMTBIT_S24_LE |\
 			                 SNDRV_PCM_FMTBIT_S32_LE),
 		},
-		.ops = &tas5825m_speaker_dai_ops,
+		.ops = &tas5825m_dai_ops,
 	},
 };
 
-static bool tas5825m_is_volatile_reg(struct device *dev, unsigned int reg)
-{
-	return true;
-}
 
-static bool tas5825m_is_writable_reg(struct device *dev, unsigned int reg)
-{
-	return true;
-}
-
-static const struct regmap_config tas5825m_regmap_config = {
-	.reg_bits = 8,
-	.val_bits = 8,
-
-	.writeable_reg = tas5825m_is_writable_reg,
-	.volatile_reg = tas5825m_is_volatile_reg,
-
-	.cache_type = REGCACHE_RBTREE,
-	.max_register = 128,
-};
-
-static int tas5825m_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
+static int tas5825m_i2c_probe(struct i2c_client *i2c,
+                          const struct i2c_device_id *id)
 {
 	struct tas5825m_priv *priv;
-	struct device *dev = &client->dev;
-	int i, ret;
+	struct device *dev = &i2c->dev;
+	size_t i;
+	int ret;
 
 	dev_dbg(dev, "%s()\n", __func__);
 
-	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	priv = devm_kzalloc(dev, sizeof(struct tas5825m_priv), GFP_KERNEL);
 	if (!priv)
 		return -ENOMEM;
 
 	dev_set_drvdata(dev, priv);
 
-	mutex_init(&priv->lock);
-	priv->volume_l_index = TAS5825M_VOLUME_DEFAULT_INDEX;
-	priv->volume_r_index = TAS5825M_VOLUME_DEFAULT_INDEX;
-	priv->muted = false;
-	priv->regmap = devm_regmap_init_i2c(client, &tas5825m_regmap_config);
-	if (IS_ERR(priv->regmap))
-		return PTR_ERR(priv->regmap);
+	priv->regmap = devm_regmap_init_i2c(i2c, &tas5825m_regmap_config);
+	if (IS_ERR(priv->regmap)) {
+		ret = PTR_ERR(priv->regmap);
+		dev_err(dev, "Failed to allocate regmap: %d\n", ret);
+		return ret;
+	}
 
 	for (i = 0; i < TAS5825M_NUM_SUPPLIES; i++)
 		priv->supplies[i].supply = tas5825m_supply_names[i];
@@ -403,20 +290,27 @@ static int tas5825m_i2c_probe(struct i2c_client *client, const struct i2c_device
 
 	msleep(100);
 
-	ret = regmap_register_patch(priv->regmap,
-	                            tas5825m_init_sequence, ARRAY_SIZE(tas5825m_init_sequence));
-	if (ret != 0) {
-		dev_err(dev, "failed to start initialization: %d\n", ret);
-		return ret;
-	}
+	/*
+	we don't use regmap_register_patch because that would conflict with
+	the register access restrictions we configure in regmap_config
+	*/
+	for (i = 0; i < ARRAY_SIZE(tas5825m_init_sequence); i++) {
+		struct reg_sequence reg = tas5825m_init_sequence[i];
+		uint8_t buffer[2] = { reg.reg, reg.def };
+		ret = i2c_master_send(i2c, buffer, 2);
+		if (reg.delay_us > 0) {
+			udelay(reg.delay_us);
+		}
 
-	ret = regmap_async_complete(priv->regmap);
-	if (ret != 0) {
-		dev_err(dev, "failed to complete initialization: %d\n", ret);
-		return ret;
-	}
+		if (ret == 2)
+			continue;
 
-	msleep(500);
+		dev_err(dev, "failed to initialize: %d\n", ret);
+		if (ret < 0)
+			return ret;
+		else
+			return -EIO;
+	}
 
 	ret = snd_soc_register_codec(dev,
 	                             &soc_codec_dev_tas5825m,
@@ -428,6 +322,7 @@ static int tas5825m_i2c_probe(struct i2c_client *client, const struct i2c_device
 
 	return 0;
 }
+
 
 static int tas5825m_i2c_remove(struct i2c_client *client)
 {
@@ -442,6 +337,7 @@ static int tas5825m_i2c_remove(struct i2c_client *client)
 	return 0;
 }
 
+
 #if IS_ENABLED(CONFIG_OF)
 static const struct of_device_id tas5825m_of_match[] = {
 	{ .compatible = "ti,tas5825m", },
@@ -450,24 +346,24 @@ static const struct of_device_id tas5825m_of_match[] = {
 MODULE_DEVICE_TABLE(of, tas5825m_of_match);
 #endif
 
-static const struct i2c_device_id tas5825m_i2c_id[] = {
-	{ "tas5825m", },
+static const struct i2c_device_id tas5825m_i2c_ids[] = {
+	{ "tas5825m", 0 },
 	{ }
 };
-MODULE_DEVICE_TABLE(i2c, tas5825m_i2c_id);
+MODULE_DEVICE_TABLE(i2c, tas5825m_i2c_ids);
 
 static struct i2c_driver tas5825m_i2c_driver = {
 	.driver = {
 		.name = "tas5825m",
 		.of_match_table = of_match_ptr(tas5825m_of_match),
 	},
+	.id_table = tas5825m_i2c_ids,
 	.probe = tas5825m_i2c_probe,
 	.remove = tas5825m_i2c_remove,
-	.id_table = tas5825m_i2c_id,
 };
 
 module_i2c_driver(tas5825m_i2c_driver);
 
 MODULE_DESCRIPTION("ASoC TAS5825M driver");
-MODULE_AUTHOR("Andy Liu <andy-liu@ti.com>");
-MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Ali Sabil <ali.sabil@koperadev.com>");
+MODULE_LICENSE("GPL");
